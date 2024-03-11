9f01a2a7e76ec058fc5c487c279d1e1beb4fc056
Chung Joo Young <137844085+cjy1947@users.noreply.github.com>
2024-03-11 22:12:00 +0900

    Update 전화번호 목록.py

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,13 @@
 def solution(phone_book):
     hash_map = {}
-    for nums in phone_book:
+    for nums in phone_book: //전화번호를 해시맵에 추가
         hash_map[nums] =1
     
-    for nums in phone_book:
+    for nums in phone_book: 
         arr=""
-        for n in nums :
+        for n in nums : //번호의 각 숫자를 arr에 추가
             arr += n
             
-            if arr in hash_map and arr != nums:
+            if arr in hash_map and arr != nums: //arr이 접두어이면 false반환
                 return False
     return True
